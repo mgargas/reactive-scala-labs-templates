218875fe74675da6f386db5a4e329f713b55abd9
mgargas <250497@wp.pl>
2019-11-13 21:48:29 +0100

    implement PersistentCheckout

--- src/main/scala/EShop/lab4/PersistentCheckout.scala
+++ src/main/scala/EShop/lab4/PersistentCheckout.scala
@@ -1,14 +1,11 @@
 package EShop.lab4
 
-import EShop.lab2.CartActor
 import EShop.lab3.Payment
 import akka.actor.{ActorRef, Cancellable, Props}
 import akka.event.{Logging, LoggingReceive}
-import akka.persistence.PersistentActor
+import akka.persistence.{PersistentActor, SnapshotOffer}
 
-import scala.util.Random
 import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
 
 object PersistentCheckout {
 
@@ -22,33 +19,65 @@ class PersistentCheckout(
 ) extends PersistentActor {
 
   import EShop.lab2.Checkout._
+  val timerDuration     = 1.seconds
   private val scheduler = context.system.scheduler
   private val log       = Logging(context.system, this)
-  val timerDuration     = 1.seconds
 
-  private def updateState(event: Event, maybeTimer: Option[Cancellable] = None): Unit = {
-    ???
-    event match {
-      case CheckoutStarted                => ???
-      case DeliveryMethodSelected(method) => ???
-      case CheckOutClosed                 => ???
-      case CheckoutCancelled              => ???
-      case PaymentStarted(payment)        => ???
-
-    }
+  override def receiveCommand: Receive = LoggingReceive.withLabel("State: receiveCommand") {
+    case StartCheckout => persist(CheckoutStarted)(event => updateState(event))
   }
 
-  def receiveCommand: Receive = ???
+  def selectingDelivery(timer: Cancellable): Receive = LoggingReceive.withLabel("[State: selectingDelivery]") {
+    case CancelCheckout => persist(CheckoutCancelled)(event => updateState(event, Some(timer)))
+    case ExpireCheckout => persist(CheckoutCancelled)(event => updateState(event, Some(timer)))
+    case SelectDeliveryMethod(method) =>
+      persist(DeliveryMethodSelected(method))(event => updateState(event, Some(timer)))
+  }
 
-  def selectingDelivery(timer: Cancellable): Receive = ???
+  def selectingPaymentMethod(timer: Cancellable): Receive = LoggingReceive.withLabel("[State: processingPayment]") {
+    case CancelCheckout => persist(CheckoutCancelled)(event => updateState(event, Some(timer)))
+    case ExpireCheckout => persist(CheckoutCancelled)(event => updateState(event, Some(timer)))
+    case SelectPayment(payment) =>
+      val paymentActor = context.actorOf(Payment.props(payment, sender(), self))
+      sender() ! PaymentStarted(paymentActor)
+      persist(PaymentStarted(paymentActor))(event => updateState(event))
+  }
 
-  def selectingPaymentMethod(timer: Cancellable): Receive = ???
+  def processingPayment(timer: Cancellable): Receive = LoggingReceive.withLabel("[State: processingPayment]") {
+    case CancelCheckout => persist(CheckoutCancelled)(event => updateState(event, Some(timer)))
+    case ExpireCheckout => persist(CheckoutCancelled)(event => updateState(event, Some(timer)))
+    case ReceivePayment =>
+      cartActor ! CheckOutClosed
+      persist(CheckOutClosed)(event => updateState(event, Some(timer)))
+  }
 
-  def processingPayment(timer: Cancellable): Receive = ???
+  def cancelled: Receive = LoggingReceive.withLabel("[State: cancelled]") {
+    case _ => log.info("Checkout already cancelled")
+  }
 
-  def cancelled: Receive = ???
+  def closed: Receive = LoggingReceive.withLabel("[State: closed]") {
+    case _ => log.info("Checkout already closed")
+  }
 
-  def closed: Receive = ???
+  override def receiveRecover: Receive = {
+    case event: Event     => updateState(event)
+    case _: SnapshotOffer => log.error("Received unhandled snapshot offer")
+  }
 
-  override def receiveRecover: Receive = ???
+  private def scheduleCheckoutTimer: Cancellable =
+    scheduler.scheduleOnce(timerDuration, self, ExpireCheckout)(context.system.dispatcher)
+
+  private def schedulePaymentTimer: Cancellable =
+    scheduler.scheduleOnce(timerDuration, self, ExpirePayment)(context.system.dispatcher)
+
+  private def updateState(event: Event, maybeTimer: Option[Cancellable] = None): Unit = {
+    maybeTimer.foreach(_.cancel())
+    event match {
+      case CheckoutStarted           => context become selectingDelivery(scheduleCheckoutTimer)
+      case DeliveryMethodSelected(_) => context become selectingPaymentMethod(scheduleCheckoutTimer)
+      case CheckOutClosed            => context become closed
+      case CheckoutCancelled         => context become cancelled
+      case PaymentStarted(_)         => context become processingPayment(schedulePaymentTimer)
+    }
+  }
 }
