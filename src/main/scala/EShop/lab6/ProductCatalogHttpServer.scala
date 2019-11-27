221efa070f157beb373ed192637452e3bb6c8fc1
mgargas <250497@wp.pl>
2019-11-27 19:46:03 +0100

    add router with productCatalog workers

--- src/main/scala/EShop/lab6/ProductCatalogHttpServer.scala
+++ src/main/scala/EShop/lab6/ProductCatalogHttpServer.scala
@@ -1,25 +1,25 @@
-package EShop.lab5
+package EShop.lab6
 
+import EShop.lab5.{ProductCatalog, SearchService}
 import EShop.lab5.ProductCatalog.{GetItems, Items}
 import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.StatusCodes.{InternalServerError, OK}
 import akka.http.scaladsl.server.{HttpApp, Route}
 import akka.pattern.ask
+import akka.routing.RoundRobinPool
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
-class ProductCatalogHttpServer(system: ActorSystem) extends HttpApp with SprayJsonSupport with JsonSupport {
+class ProductCatalogHttpServer(system: ActorSystem, productCatalogSystem: ActorSystem) extends HttpApp with SprayJsonSupport with JsonSupport {
 
   implicit val getItmesFormat = jsonFormat2(ProductCatalog.GetItems)
   implicit val itemFormat     = jsonFormat5(ProductCatalog.Item)
   implicit val itemsFormat    = jsonFormat1(ProductCatalog.Items)
-  import system.dispatcher
   implicit val timeout: Timeout = 5.seconds
-  private val productCatalog =
-    system.actorSelection("akka.tcp://ProductCatalog@127.0.0.1:2553/user/productcatalog").resolveOne()
+  private val workers = productCatalogSystem.actorOf(RoundRobinPool(9).props(ProductCatalog.props(new SearchService())))
 
   override protected def routes: Route = pathPrefix("query") {
     post {
@@ -32,7 +32,7 @@ class ProductCatalogHttpServer(system: ActorSystem) extends HttpApp with SprayJs
   }
 
   private def getItems(request: GetItems): Route = {
-    onComplete(productCatalog.flatMap(actor => (actor ? request).mapTo[Items])) {
+    onComplete((workers ? request).mapTo[Items]) {
       case Failure(exception) =>
         system.log.error(exception.getMessage)
         complete(InternalServerError)
@@ -46,5 +46,9 @@ object ProductCatalogHttpServer extends App {
   private val system          = ActorSystem("ProductCatalogHttp", config.getConfig("catalogserver").withFallback(config))
   private val address: String = config.getString("catalogserver.address")
   private val port: Int       = config.getInt("catalogserver.port")
-  new ProductCatalogHttpServer(system).startServer(address, port)
+  private val productCatalogSystem = ActorSystem(
+    "ProductCatalog",
+    config.getConfig("productcatalog").withFallback(config)
+  )
+  new ProductCatalogHttpServer(system, productCatalogSystem).startServer(address, port)
 }
